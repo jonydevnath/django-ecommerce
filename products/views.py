fff7294ca1b1c1d673bd44109a81c07c46424b74
jonydevnath <76802139+jonydevnath@users.noreply.github.com>
2026-01-31 23:55:07 +0600

    Day 6: Implement product list view, template, and URL configuration

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    # 1. Fetch data from DB
+    products = Product.objects.filter(is_active = True)
+
+    # 2. Package data into a "context" dictionary
+    context = {
+        'products': products
+    }
+
+    # 3. Render HTML with data
+    return render(request, 'products/product_list.html', context)
